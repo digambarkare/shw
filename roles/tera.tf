7c01ed8624b0fe6d04b99412d9179d34ae4c3ca4
digambarkare <digambark24@gmail.com>
2024-03-12 21:06:00 +0530

    --- Auto Git Commit ---

--- roles/tera.tf
+++ roles/tera.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1"
   shared_shared_config_files = [/home/root/.aws/config]
-  shared_credentials_files   = [/root/.aws/credentials]
+  shared_credentials_files   = [/home/root/.aws/credentials]
   profile = digambar
 }
 # Create a instaunce
