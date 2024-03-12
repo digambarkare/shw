f490008d13ecd72e7890a425cee8567f25cf0c28
digambarkare <digambark24@gmail.com>
2024-03-12 21:02:28 +0530

    --- Auto Git Commit ---

---
+++ roles/tera.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  shared_shared_config_files = [/root/.aws/config]
+  shared_credentials_files   = [/root/.aws/credentials]
+  profile = digambar
+}
+# Create a instaunce
+resource "aws_instunce" "my-instaunce" {
+  ami = "ami-0123c9b6bfb7eb962"
+  instaunce_type ="t2.micro"
+  key_name ="digambar"
+}
