25d6532da74281d170d8e40b46e68d447041a7a3
digambarkare <digambark24@gmail.com>
2024-03-14 20:43:30 +0530

    add

--- roles/tera.tf
+++ roles/tera.tf
@@ -19,4 +19,12 @@ resource "aws_instance" "digu" {
   ami = "ami-0123c9b6bfb7eb962"
   instance_type ="t2.micro"
   key_name ="digambar"
+  tags = {
+    Name = "jarvis-server"
+    Enviorment ="staging"
+    Project ="xyz"
+  }
 }
+
+  
+
