810197c95e4de3b7d882401910622f5fbc330442
digambarkare <digambark24@gmail.com>
2024-03-16 00:24:55 +0530

    --- Auto Git Commit ---

---
+++ vpc3.tf
@@ -0,0 +1,7 @@
+# Configure the AWS Provider
+provider "aws" {
+    region = "ap-southeast-1"
+    shared_config_files        = ["/root/.aws/config"]
+    shared_credentials_files   =["/root/.aws/credentials"]
+    profile                    = "digambar"
+  }
