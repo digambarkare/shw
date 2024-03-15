d2ab4df4288ddf6d1375a5218633ee3f4ce47a8f
digambarkare <digambark24@gmail.com>
2024-03-15 23:45:24 +0530

    --- Auto Git Commit ---

--- vpc2.tf
+++ vpc2.tf
@@ -8,7 +8,7 @@ required_providers {
 }
 
 # Configure the AWS Provider
- provider "aws" {
+provider "aws" {
     region = "ap-southeast-1"
     shared_config_files        = ["/root/.aws/config"]
     shared_credentials_files   =["/root/.aws/credentials"]
