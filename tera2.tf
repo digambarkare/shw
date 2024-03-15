8f8fef4ae0aef5485ddb33903007a148400cf37b
digambarkare <digambark24@gmail.com>
2024-03-15 21:23:26 +0530

    --- Auto Git Commit ---

--- tera2.tf
+++ tera2.tf
@@ -1,30 +1,30 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-# # Configure the AWS Provider
-# provider "aws" {
-#   region = "ap-southeast-1"
-#   shared_config_files        = ["/root/.aws/config"]
-#   shared_credentials_files   =["/root/.aws/credentials"]
-#   profile                    = "digambar"
-# }
-# # Create a instaunce
-# resource "aws_instance" "digu" {
-#   ami = "ami-0123c9b6bfb7eb962"
-#   instance_type ="t2.micro"
-#   key_name ="digambar"
-#   tags = {
-#     Name = "jarvis-server"
-#     Enviorment ="staging"
-#     Project ="xyz"
-#   }
-# }
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  shared_config_files        = ["/root/.aws/config"]
+  shared_credentials_files   =["/root/.aws/credentials"]
+  profile                    = "digambar"
+}
+# Create a instaunce
+resource "aws_instance" "digu" {
+  ami = "ami-0123c9b6bfb7eb962"
+  instance_type ="t2.micro"
+  key_name ="digambar"
+  tags = {
+    Name = "jarvis-server"
+    Enviorment ="staging"
+    Project ="xyz"
+  }
+}
 
   
 
