bb3ccc424a24efe8526b44b61b89b30981e83e50
digambarkare <digambark24@gmail.com>
2024-03-16 02:06:41 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "mybucket-1" {
-#     bucket = "myspecialbucklletyomi"
-#       tags = {
-#     Name        = "My-bucket-12"
+  bucket = "myspecialbucklletyomi"
+      tags = {
+  Name        = "My-bucket-12"
 #     Environment = "Development"
 #   }
   
