d1879caf6c273c87a1d0b96885e5cd15a1a768fb
digambarkare <digambark24@gmail.com>
2024-03-15 20:11:17 +0530

    --- Auto Git Commit ---

---
+++ s3_buck.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  shared_config_files        = ["/root/.aws/config"]
+  shared_credentials_files   =["/root/.aws/credentials"]
+  profile                    = "digambar"
+}
+resource "aws_s3_bucket" "rohanyyy00" {
+    bucket = "my-tf-test-bucket"
+
+    tags = {
+        Name = "my bucket"
+        Enviorment = "Development"
+    }    
+}   
