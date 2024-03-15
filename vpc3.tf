319f26e427c07998eb62f482442d394bbb61515d
digambarkare <digambark24@gmail.com>
2024-03-16 00:28:46 +0530

    --- Auto Git Commit ---

--- vpc3.tf
+++ vpc3.tf
@@ -20,13 +20,3 @@ resource "aws_vpc" "myvpc" {
   enable_dns_support = true
   enable_dns_hostnames = true
 }
-
-resource "aws_subnet" "my-subnet" {
-    vpc_id = "vpc-0dff66d4e2b2eb47b"
-    cidr_block = "10.0.0.0/26"
-    availability_zone_id = "use2-az1"
-    map_public_ip_on_launch = true
-    tags = {
-      Name = "mysubnet"
-    }
-}
