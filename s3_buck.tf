a4179a76f78ae608d00baa1aec155fef503a0b1b
digambarkare <digambark24@gmail.com>
2024-03-15 20:21:50 +0530

    --- Auto Git Commit ---

--- s3_buck.tf
+++ s3_buck.tf
@@ -1,9 +1,15 @@
-
-resource "aws_s3_bucket" "rohanyyy00" {
-    bucket = "my-tf-test-bucket"
-
-    tags = {
-        Name = "my bucket"
-        Enviorment = "Development"
-    }    
+ provider "aws" {
+   region = "ap-southeast-1"
+   shared_config_files        = ["/root/.aws/config"]
+   shared_credentials_files   =["/root/.aws/credentials"]
+   profile                    = "digambar"
 }   
+
+resource "aws_s3_bucket" "mybucket-1" {
+    bucket = "myspecialbucklletyomi"
+      tags = {
+    Name        = "My-bucket-12"
+    Environment = "Development"
+  }
+  
+}
