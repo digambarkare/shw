182e7c9323048edee9b21664aede06a1258f257c
digambarkare <digambark24@gmail.com>
2024-03-16 01:41:04 +0530

    --- Auto Git Commit ---

--- myfirst inst.tf
+++ myfirst inst.tf
@@ -1,8 +1,8 @@
 
 # # Create a instaunce
 # resource "aws_instance" "digu" {
-#   ami = "ami-0123c9b6bfb7eb962"
-#   instance_type ="t2.micro"
+#   ami = "ami-0123c9b6bfb7eb962="t2.micro""
+#   instance_type 
 #   key_name ="digambar"
 #   tags = {
 #     Name = "jarvis-server"
