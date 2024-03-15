991a3e54f17427770598aa9f0676dbea18ef1e0d
digambarkare <digambark24@gmail.com>
2024-03-16 00:27:25 +0530

    --- Auto Git Commit ---

--- vpc3.tf
+++ vpc3.tf
@@ -15,4 +15,9 @@ provider "aws" {
     }
 }                    
 
+resource "aws_vpc" "myvpc" {
+#   cidr_block = "10.0.0.0/24"
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+# }
 
