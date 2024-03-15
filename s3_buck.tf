6e60ae94cc63cac8e9d651083608e30e3dfc9d3d
digambarkare <digambark24@gmail.com>
2024-03-15 20:15:58 +0530

    --- Auto Git Commit ---

--- s3_buck.tf
+++ s3_buck.tf
@@ -1,12 +1,4 @@
 
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-southeast-1"
-  shared_config_files        = ["/root/.aws/config"]
-  shared_credentials_files   =["/root/.aws/credentials"]
-  profile                    = "digambar"
-}
 resource "aws_s3_bucket" "rohanyyy00" {
     bucket = "my-tf-test-bucket"
 
