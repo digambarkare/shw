0becbc083cc0bc21b7591e1798e17bd7e6326bcc
digambarkare <digambark24@gmail.com>
2024-03-16 02:10:42 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -11,5 +11,5 @@ resource "aws_s3_bucket" "mybucket-1" {
 resource "aws_s3_object" "my_object" {
   bucket = aws_s3_bucket.my_bucket.id  # Reference to the bucket created above
   key    = "tfstate/terraform.tfstate"        # Key (name) of the object in the bucket
-  source = "./terraform.tfstate" #he file you want to upload
+  source = "./terraform.tfstate" # file you want to upload
 }
