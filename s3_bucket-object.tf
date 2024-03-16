5dcbf11c421173d14b316abc86835bf984a5a2d9
digambarkare <digambark24@gmail.com>
2024-03-16 19:56:07 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#        source  = "hashicorp/aws"
-#        version = "~> 5.0"
-#      }
-#    }
-#  }
+terraform {
+  required_providers {
+    aws = {
+       source  = "hashicorp/aws"
+       version = "~> 5.0"
+     }
+   }
+ }
 
 resource "aws_s3_bucket" "mybucket-1" {
      bucket = "myspecialbucklletyomi"
