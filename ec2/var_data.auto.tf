4e3892cbac39f39d98a290b0bb8859c8c7e7b9f9
digambarkare <digambark24@gmail.com>
2024-03-16 23:00:20 +0530

    --- Auto Git Commit ---

--- ec2/var_data.auto.tf
+++ ec2/var_data.auto.tf
@@ -1,1 +1,15 @@
-# 
+# profile = "omi"
+
+region = "us-east-2"
+
+ami = "ami-0d77c9d87c7e619f9"
+
+instance_type = "t3.micro"
+
+key_name = "windows_global_key"
+
+tags = {
+    Name = "Redhat-server"
+    Enviorment = "production"
+    Project = "XYZ Project"
+}
