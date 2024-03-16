29c9c44598dab8d06d6f59b0975c2f665565078c
digambarkare <digambark24@gmail.com>
2024-03-16 23:16:24 +0530

    --- Auto Git Commit ---

--- ec2/var_data.auto.tf
+++ ec2/var_data.auto.tf
@@ -4,12 +4,12 @@
 
 # ami = "ami-0123c9b6bfb7eb962"
 
-# instance_type = "t2.micro"
+instance_type = "t2.micro"
 
 key_name = "digambar2"
 
-# tags = {
-#     Name = "jarvis-server"
-#     Enviorment = "production"
-#     Project = "XYZ Project"
-# }
+tags = {
+    Name = "jarvis-server"
+    Enviorment = "production"
+    Project = "XYZ Project"
+}
