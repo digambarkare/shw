262ec6265237aa12afa3ac7214cd3465812117f4
digambarkare <digambark24@gmail.com>
2024-03-16 21:09:37 +0530

    --- Auto Git Commit ---

--- ec2/main.tf
+++ ec2/main.tf
@@ -21,7 +21,7 @@ terraform {
     key                = "terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
-    dynamodb_table = "tera-tabale"
+    dynamodb_table = ""
   }
 }
 
