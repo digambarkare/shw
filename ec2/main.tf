1b8964bfae6e286cb6a8e507f265bf99e56b7802
digambarkare <digambark24@gmail.com>
2024-03-16 20:25:45 +0530

    --- Auto Git Commit ---

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         	   = ""
+    bucket         	   = "jdijdijdij"
     key                = "terraform.tfstate"
     region         	   = "ap-south-1"
     encrypt        	   = true
