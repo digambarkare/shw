84a4d4f97785e99c61208d1d71988dbf2d8c1891
digambarkare <digambark24@gmail.com>
2024-03-16 18:16:21 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -1,15 +1,15 @@
-# resource "aws_s3_bucket" "mybucket-1" {
-#      bucket = "myspecialbucklletyomi"
-#        tags = {
-#      Name        = "My-bucket-12"
-#      Environment = "Development"
-#    }
+resource "aws_s3_bucket" "mybucket-1" {
+     bucket = "myspecialbucklletyomi"
+       tags = {
+     Name        = "My-bucket-12"
+     Environment = "Development"
+   }
   
-#  }
+ }
 
-#  #Upload an object to the bucket
-# resource "aws_s3_object" "my_object" {
-#   bucket = aws_s3_bucket.my_bucket.id  # Reference to the bucket created above
-#   key    = "digambar/vpc.tf"        # Key (name) of the object in the bucket
-#   source = "./vpc1.tf" # file you want to upload
-# }
+ #Upload an object to the bucket
+resource "aws_s3_object" "my_object" {
+  bucket = aws_s3_bucket.my_bucket.id  # Reference to the bucket created above
+  key    = "digambar/vpc.tf"        # Key (name) of the object in the bucket
+  source = "./vpc1.tf" # file you want to upload
+}
