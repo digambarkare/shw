3fbdea1fdfa0d1c30fda00c00aa51ecb8ac03d69
digambarkare <digambark24@gmail.com>
2024-03-16 18:21:43 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "mybucket-1" {
 
  #Upload an object to the bucket
 resource "aws_s3_object" "my_object" {
-  bucket = aws_s3_bucket.my_bucket.id  # Reference to the bucket created above
+  bucket = aws_s3_bucket..id  # Reference to the bucket created above
   key    = "digambar/vpc.tf"        # Key (name) of the object in the bucket
   source = "./vpc1.tf" # file you want to upload
 }
