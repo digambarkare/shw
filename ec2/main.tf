0020243e39d4f1bc75dca3d8474701c8ec3c6ec5
digambarkare <digambark24@gmail.com>
2024-03-16 23:16:54 +0530

    --- Auto Git Commit ---

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "ap-southeast-1"
   shared_config_files        = ["/root/.aws/config"]
   shared_credentials_files   =["/root/.aws/credentials"]
-  # profile                    = "digambar"
+  profile                    = "digambar"
 }
 
 terraform {
