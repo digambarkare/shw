7dbf71322d66cf322eb61937c49f577e7bfe1333
digambarkare <digambark24@gmail.com>
2024-03-16 23:15:53 +0530

    --- Auto Git Commit ---

--- ec2/var_data.auto.tf
+++ ec2/var_data.auto.tf
@@ -1,12 +1,12 @@
 #  profile = "digambar"
 
-region = "ap-southeast-1"
+# region = "ap-southeast-1"
 
-ami = "ami-0123c9b6bfb7eb962"
+# ami = "ami-0123c9b6bfb7eb962"
 
-instance_type = "t2.micro"
+# instance_type = "t2.micro"
 
-key_name = "digambar2"
+# key_name = "digambar2"
 
 tags = {
     Name = "jarvis-server"
