f5c9e371313e046c5690f22bbf6ba66f369826b5
digambarkare <digambark24@gmail.com>
2024-03-16 20:16:53 +0530

    --- Auto Git Commit ---

---
+++ ec2/variable.tf
@@ -0,0 +1,17 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+
+# # Configure the AWS Provider
+# provider "aws" {
+#   region = "ap-southeast-1"
+#   shared_config_files        = ["/root/.aws/config"]
+#   shared_credentials_files   =["/root/.aws/credentials"]
+#   profile                    = "digambar"
+# }
