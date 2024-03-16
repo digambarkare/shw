cd0eb9f1b0228e7980b7de1f9411ab1b7a2ba509
digambarkare <digambark24@gmail.com>
2024-03-16 19:57:58 +0530

    --- Auto Git Commit ---

--- s3_bucket-object.tf
+++ s3_bucket-object.tf
@@ -8,6 +8,14 @@ terraform {
  }
 
 
+ #Configure the AWS Provider
+# provider "aws" {
+#     region = "ap-southeast-1"
+#     shared_config_files        = ["/root/.aws/config"]
+#     shared_credentials_files   =["/root/.aws/credentials"]
+#     profile                    = "digambar"
+#   }
+
 resource "aws_s3_bucket" "mybucket-1" {
      bucket = "myspecialbucklletyomi"
        tags = {
