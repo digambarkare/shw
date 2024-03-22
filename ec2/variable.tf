2997c7e79c00d816774e6f84ccea34b7183590fa
digambarkare <digambark24@gmail.com>
2024-03-22 15:10:41 +0530

    --- Auto Git Commit ---

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,22 +1,22 @@
-variable "ami" {
-    type = string
+# variable "ami" {
+#     type = string
 
-}
+# }
 
-variable "instance_type" {
-    type = string
+# variable "instance_type" {
+#     type = string
    
-}
+# }
 
-variable "key_name" {
-    type = string
+# variable "key_name" {
+#     type = string
    
-}
+# }
 
-variable "tags" {
-    type = map
+# variable "tags" {
+#     type = map
 
-}
+# }
            
 
   
