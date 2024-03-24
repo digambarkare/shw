2e0b5eef5e672591413ffad6e55a5d99a9ca6871
digambarkare <digambark24@gmail.com>
2024-03-24 10:49:15 +0530

    --- Auto Git Commit ---

--- module/main.tf
+++
@@ -1,8 +0,0 @@
-# here we create only resources and providers are defined into the projet's module main.tf file
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.cidr_block
-  tags = {
-    Name = "${var.project}-vpc"
-    enviorment = var.enviorment  
-  }
-}
