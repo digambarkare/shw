76615add1740c0a65ad5ec08150c2385829caa64
digambarkare <digambark24@gmail.com>
2024-04-08 11:47:00 +0530

    --- Auto Git Commit ---

--- student.groovy
+++ student.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/digambarkare/shw.git'
+                 
                 echo 'pull'
             }
         }
