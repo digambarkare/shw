75f622975c72bf2946ea4c0ef70ca28a4c837fa8
digambarkare <digambark24@gmail.com>
2024-04-15 19:52:41 +0530

    --- Auto Git Commit ---

--- student.groovy
+++ student.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('build') {
             steps {
-                
+                sh 'mvn clean package'
                 echo 'build done'
             }
         }
