2e71670815560cc92c878930b574131f412530b7
digambarkare <digambark24@gmail.com>
2024-04-15 19:37:28 +0530

    --- Auto Git Commit ---

--- student.groovy
+++ student.groovy
@@ -16,6 +16,10 @@ pipeline {
         }
         stage('quality-test') {
             steps {
+                sh '''mvn sonar:sonar \\
+  -Dsonar.projectKey=student.api \\
+  -Dsonar.host.url=http://13.212.23.53:9000 \\
+  -Dsonar.login=84a7d5fb5fcc3092d2216dcd99d0caad31041dad'''
                 echo 'quality-test'
             }
         }
