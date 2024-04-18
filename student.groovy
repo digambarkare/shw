45bbd841fa29f85ba8450476ad267fd8dbac1dd2
digambarkare <digambark24@gmail.com>
2024-04-18 17:02:18 +0530

    jj

--- student.groovy
+++ student.groovy
@@ -1,26 +1,15 @@
+//my pipeline
 pipeline {
-    agent any
     stages {
         stage('pull') {
             steps {
-                git branch: 'main', credentialsId: 'tttttt', url: 'https://github.com/digambarkare/shw'
-                echo 'pull'
-                echo 'hi baby'
+               git branch: 'main', url: 'https://github.com/digambarkare/shw.git'
+                echo 'pull done'
             }
         }
         stage('build') {
             steps {
-                sh '/opt/maven/mvn clean package'
                 echo 'build done'
-            }
-        }
-        stage('quality-test') {
-            steps {
-                sh '''mvn sonar:sonar \\
-  -Dsonar.projectKey=student.api \\
-  -Dsonar.host.url=http://13.212.23.53:9000 \\
-  -Dsonar.login=84a7d5fb5fcc3092d2216dcd99d0caad31041dad'''
-                echo 'quality-test'
             }
         }
         stage('test') {
@@ -28,9 +17,9 @@ pipeline {
                 echo 'test done'
             }
         }
-        stage('deploy') {
+            stage('deploy test') {
             steps {
-                echo 'deploy done'
+                echo 'quality test done'
             }
         }
     }
