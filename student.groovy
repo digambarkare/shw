e149586ffc31ad5268b2b498c1bcf9381388f760
digambarkare <digambark24@gmail.com>
2024-04-18 16:53:17 +0530

    HHh

--- student.groovy
+++ student.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-                git branch: 'main', credentialsId: 'tttttt', url: 'https://github.com/digambarkare/shw.git'
+                git branch: 'main', credentialsId: 'tttttt', url: 'https://github.com/digambarkare/shw'
                 echo 'pull'
                 echo 'hi baby'
             }
