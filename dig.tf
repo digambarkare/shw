59ea55a5a76724b116d6cf9dca504009bf85ca32
digambark24@gmail.com <karedigambar88@gmail.com>
2024-08-01 23:24:31 +0530

    jj

--- dig.tf
+++ dig.tf
@@ -1,1 +1,20 @@
-ijci
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-0be811527bbd59596"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
